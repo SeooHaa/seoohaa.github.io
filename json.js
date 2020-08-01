463d6136b0bcd2258ceff2075fa542a1d4c630cd
x19054xx <x19054xx@aitech.ac.jp>
2020-08-02 00:09:47 +0900

    iidcs

--- json.js
+++ json.js
@@ -17,12 +17,87 @@ if(!navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)){
 }
 
 $(function() {
-      $('#slideR').hover(
+      $('.slideR').hover(
       function(){
-        $(this).animate({'marginRight':'140px'},500);
+        $(this).animate({'marginRight':'90px'},500);
       },
       function () {
         $(this).animate({'marginRight':'0'},500);
       }
     );
+});
+
+$(function() {
+  $('#page1').hover(
+  function(){
+    $('body').animate({
+      backgroundColor: "#64b397"
+      },400),
+      $('.slideR').animate({
+        backgroundColor: "#a1d4c3","border-left-color": "#FFF"},400),
+        $('.iconpr').animate({
+          opacity: "100%"},400
+          )
+    },
+    function(){
+      $('body').animate({
+        backgroundColor: "#FFF"
+      },400),
+      $('.slideR').animate({
+        backgroundColor: "","border-left-color": "gray"},400),
+        $('.iconpr').animate({
+          opacity: "0%"},400
+          )
+    }
+  );
+});
+
+$(function() {
+  $('#page2').hover(
+  function(){
+    $('body').animate({
+      backgroundColor: "#ffe199"
+      },400),
+      $('.slideR').animate({
+        backgroundColor: "#ffeec7","border-left-color": "#FFF"},400),
+      $('.iconwo').animate({
+        opacity: "100%"},400
+        )
+    },
+    function(){
+      $('body').animate({
+        backgroundColor: "#FFF"
+      },400),
+      $('.slideR').animate({
+        backgroundColor: "","border-left-color": "gray"},300),
+      $('.iconwo').animate({
+        opacity: "0%"},400
+        )
+    }
+  );
+});
+
+$(function() {
+  $('#page3').hover(
+  function(){
+    $('body').animate({
+      backgroundColor: "#b5b5b5"
+      },400),
+      $('.slideR').animate({
+        backgroundColor: "#d3d3d3","border-left-color": "#FFF"},300),
+        $('.iconhe').animate({
+          opacity: "100%"},400
+          )
+    },
+    function(){
+      $('body').animate({
+        backgroundColor: "#FFF"
+      },300),
+      $('.slideR').animate({
+        backgroundColor: "","border-left-color": "gray"},400),
+        $('.iconhe').animate({
+          opacity: "0%"},400
+          )
+    }
+  );
 });
